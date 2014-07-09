9daa8c82191032fbf1a4bfd847680466a6c3d1ba
liuliming <liuliming@xinnet.com>
2014-07-09 10:21:16 +0800

    init

--- pages/test.php
+++ pages/test.php
@@ -1,15 +1,8 @@
-<?php
-/**
- * A test page for theme developer to view the layout of an email notification
- */
+<?php 
 
-admin_gatekeeper();
-
-$title = elgg_echo("useradd:subject");
-$message = elgg_echo("useradd:body");
-
-echo elgg_view("html_email_handler/notification/body", array(
-	"subject" => $title,
-	"body" => $message,
-	"recipient" => elgg_get_logged_in_user_entity()
-));
+	admin_gatekeeper();
+	
+	$title = elgg_echo("useradd:subject");
+	$message = elgg_echo("useradd:body");
+	
+	echo elgg_view("html_email_handler/notification/body", array("title" => $title, "message" => $message));
